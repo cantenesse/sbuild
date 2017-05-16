cfe94c25571e44b91c8cafa682d2c6afce1cb34d
Chris Antenesse <chris@antenesse.net>
2017-05-16 16:33:53 -0700

    fixes get_secret

---
+++ lib/sbuild.rb
@@ -0,0 +1,72 @@
+require 'open3'
+
+def self.safe_retry(cmd, tries=3)
+    begin
+        status, output = system_safe(cmd)
+    rescue
+        if (tries -= 1) > 0
+            sleep 5
+            retry unless (tries).zero?
+        else
+            raise "Build failed"
+        end
+    end
+
+    return status
+
+end
+def self.system_safe(cmd)
+    cmd_sanitized = get_sanitized_cmd(cmd)
+    puts "Executing command: #{cmd_sanitized}"
+
+    stdout, stderr, status = Open3.capture3(cmd)
+
+    puts "STDOUT:\n#{stdout}"
+    puts "STDERR:\n#{stderr}"
+
+    if not status.success?
+        raise "Build failed"
+    end
+
+    return [status.success?, stdout]
+
+end
+
+def self.get_secret(key, subkey)
+    cmd = "vault read -field=#{subkey} #{key}"
+    status, secret = system_safe(cmd)
+	secret = secret.strip
+end
+
+def self.revoke_lease(lease_id)
+	cmd = "vault revoke #{lease_id}"
+	safe_retry(cmd)
+end
+
+def self.get_sanitized_cmd(cmd)
+    secret_fields = ["AWS_ACCESS_KEY_ID",
+                     "AWS_SECRET_ACCESS_KEY",
+                     "password",
+                     "aws_access_key_id",
+                     "aws_secret_access_key",
+                     "aws_access_key",
+                     "aws_secret_key",
+                     "access_key",
+                     "secret_key",
+                     "account_access_key",
+                     "account_secret_key",
+                     "aws.accessKeyId",
+                     "aws.secretKey",
+                     "jwtsecret",
+                     "master_access_key",
+                     "master_secret_key",
+                     "github_password"]
+
+    cmd = cmd.gsub(/\s+/m, ' ').strip.split(" ")
+    replace = cmd.grep(Regexp.union(secret_fields))
+    unless replace.empty?
+        replace = Set.new replace
+        cmd.collect! {|e| (replace.include? e) ? '<secret>': e}
+    end
+    cmd = cmd.join(' ')
+end
---
+++ sbuild.gemspec
@@ -0,0 +1,13 @@
+Gem::Specification.new do |s|
+  s.name        = "sbuild"
+  s.version     = "0.0.2"
+  s.date        = "2017-05-16"
+  s.summary     = "Build library"
+  s.description = "Common functions for build"
+  s.authors     = ["Chris Antenesse"]
+  s.email       = "chrisantenesse@gmail.com"
+  s.files       = ["lib/sbuild.rb"]
+  s.homepage    =
+    'http://rubygems.org/gems/socotra'
+  s.license       = "MIT"
+end
